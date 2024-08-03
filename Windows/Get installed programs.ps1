21ebd70fd636e882da607d7ac2ae080761b2e4d5
Mark Wilbrink <markwilbrink86@gmail.com>
2024-08-03 11:04:28 +0200

    Add more reg keys

--- Windows/Get installed programs.ps1
+++ Windows/Get installed programs.ps1
@@ -1,6 +1,11 @@
 $Apps_WMI = Get-WmiObject -Class Win32_Product | Where-Object Name -ne $null
-$Apps_Reg_x64 = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object DisplayName -ne $null
-$Apps_Reg_x86 = Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object DisplayName -ne $null
+$Apps_Reg = @(
+	"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+	"HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
+	"HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+	"HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+)
+$Apps_RegKey = Get-ChildItem $Apps_Reg | Get-ItemProperty | Where-Object DisplayName -ne $null
 
 $Complete_List = @()
 
@@ -13,20 +18,11 @@ Foreach ($App_WMI in $Apps_WMI)
 	$Complete_List += $List
 }
 
-Foreach ($App_Reg_x64 in $Apps_Reg_x64)
+Foreach ($App_RegKey in $Apps_RegKey)
 {
 	$List = New-Object -TypeName PSObject
-	$List | Add-Member -NotePropertyName Name -NotePropertyValue $App_Reg_x64.DisplayName
-	$List | Add-Member -NotePropertyName Version -NotePropertyValue $App_Reg_x64.DisplayVersion
-
-	$Complete_List += $List
-}
-
-Foreach ($App_Reg_x86 in $Apps_Reg_x86)
-{
-	$List = New-Object -TypeName PSObject
-	$List | Add-Member -NotePropertyName Name -NotePropertyValue $App_Reg_x86.DisplayName
-	$List | Add-Member -NotePropertyName Version -NotePropertyValue $App_Reg_x86.DisplayVersion
+	$List | Add-Member -NotePropertyName Name -NotePropertyValue $App_RegKey.DisplayName
+	$List | Add-Member -NotePropertyName Version -NotePropertyValue $App_RegKey.DisplayVersion
 
 	$Complete_List += $List
 }
